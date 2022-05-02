7335cb99cb123d0c0d9438107a27733f3507fa9a
da175david <101520497+da175david@users.noreply.github.com>
2022-05-02 18:25:16 -0500

    Add files via upload

---
+++ ejercicio 5/ejercicio5.1.js
@@ -0,0 +1,5 @@
+
+
+var numbers=[1,4,6,10,22,55,46,2,5,0];
+numbers.sort(function(a,b){return a-b});
+numbers.slice(3);
---
+++ ejercicio 6/ejercicio 6.js
@@ -0,0 +1,7 @@
+
+
+var animals=["Eagle","Parrot","Monkey","Boar","Lion"];
+var comingSoonAnimals=["Panter","Dragon","Turtle"];
+var general=animals.concat(comingSoonAnimals);
+general.push("Cow");
+general.shift();
