ee9ec49bccae2bd38fb1969cd6f5e397102a491d
da175david <101520497+da175david@users.noreply.github.com>
2022-05-02 21:37:22 -0500

    Add files via upload

---
+++ ejercicio 7/ejercicio 7.1.js
@@ -0,0 +1,12 @@
+
+var oration=("Hola a todos, esto es una prueba")
+
+
+function words(todos) {
+    for(oration) {
+        if (todos) {
+            return true
+        }
+    }
+}
+oration(dia)
